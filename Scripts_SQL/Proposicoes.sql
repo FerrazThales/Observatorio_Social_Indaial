8742d759885aea74d2da41030565b4291eaa3133
unknown <thalesdefreitasferraz@gmail.com>
2023-03-11 18:02:53 -0300

    atualizando código

---
+++ Scripts_SQL/Proposicoes.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Proposicoes (
+	DataReuniao DATE,
+	DataDeliberacao DATE,
+	Situacao VARCHAR(200),
+	Assunto VARCHAR(1000),
+	Autor VARCHAR(1000),
+	Proposicao INT,
+	Ano INT,
+	Texto VARCHAR (MAX)
+
+)
